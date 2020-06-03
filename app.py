e4f4374464a2f6c885910eb41b8066759d98002f
Manny <mquizhpidev@gmail.com>
2020-06-02 22:09:59 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+#
+# Functions with python
+#
+
+# Creating a function
+
+def city():
+    print("New York City")
+
+city()
+
+# Passing a parameter into a function
+
+def my_city(name):
+    print(name)
+
+my_city("NYC")
+
+# Creating a function that adds two numbers together and returns the total
+
+def add_nums(num1, num2):
+    return num1 + num2
+
+total = add_nums(4, 3)
+
+print(total)
+
+
+
