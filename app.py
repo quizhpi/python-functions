2e08d8042ed3622f35304f13b676b410f556f6e2
Manny <mquizhpidev@gmail.com>
2020-06-03 21:52:31 -0400

    max_num function returns the greatest number of 3 values

--- app.py
+++ app.py
@@ -25,5 +25,21 @@ total = add_nums(4, 3)
 
 print(total)
 
+# Function that returns the greatest number
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
 
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+max_value = max_num(num1, num2, num3)
+
+print(max_value)
